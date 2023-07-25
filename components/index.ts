59a3f8b506f861990fcb2e2b00e3783b055783b6
aman-singh1601 <aman16singh2001@gmail.com>
2023-07-26 01:47:32 +0530

    added carcard

--- components/index.ts
+++ components/index.ts
@@ -4,4 +4,5 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import SearchBar from './SearchBar';
 import CustomFilter from './CustomFilter';
-export {Hero,Navbar,Footer,CustomButton,SearchBar,CustomFilter}
+import CarCard from './CarCard';
+export {Hero,Navbar,Footer,CustomButton,SearchBar,CustomFilter,CarCard}
