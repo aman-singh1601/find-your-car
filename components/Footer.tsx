c608368c69c805d146f112bdec3bed3dafb2430f
aman-singh1601 <aman16singh2001@gmail.com>
2023-07-25 17:51:15 +0530

    added searchfilter to the page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -37,7 +37,17 @@ const Footer = () => {
                     ))
                 }
             </div>
-        </div>
+            </div>
+            <div className="flex justify-between items-center border-t border-gray-100 mt-10 sm:px-16 px-6 py-10">
+                <p>@2023 CarHub. All Rights Reserved</p>
+                <div className='footer__copyrights-link'>
+                    <Link href='/' 
+                     className='text-gray-500'>
+                        Terms of Use
+                     </Link>
+                </div>
+            </div>
+        
 
     </footer>
   )
--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,6 @@ import Hero from './Hero';
 import CustomButton from './CustomButton';
 import Navbar from './Navbar';
 import Footer from './Footer';
-export {Hero,Navbar,Footer,CustomButton}
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+export {Hero,Navbar,Footer,CustomButton,SearchBar,CustomFilter}
